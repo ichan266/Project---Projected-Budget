1291f31898102f95550178789f4c93aeb01996c9
Iris Kuhn <ichan266@hotmail.com>
2020-11-28 01:20:05 -0800

    clean up some code in account_details.html

--- static/features.js
+++ static/features.js
@@ -39,7 +39,7 @@ for (const item of $('.amount')) {
   });
 };
 
-for (const item of $('.edit_form')) {
+for (const item of $('#amount_form')) {
   $(item).submit( (evt) => {
     evt.preventDefault();
     const formInputs = $(evt.target).serialize();
